36c370103db9c0406b30654d3ae55092acdc3ad5
Grigoreva Vasilisa <grigvasilisa@gmail.com>
2024-02-22 13:54:29 +0300

    add security check

---
+++ homework6/src/main/java/com/harbour/space/grigoreva/homework6/security/BasicAuthentification.java
@@ -0,0 +1,50 @@
+package com.harbour.space.grigoreva.homework6.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class BasicAuthentification {
+    @Value("${app.user.courier.username}")
+    private String username;
+    @Value("${app.user.courier.password}")
+    private String password;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsCourierService() {
+        PasswordEncoder encoder = passwordEncoder();
+        UserDetails user = User.withUsername(username)
+                .password(encoder.encode(password))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
+
